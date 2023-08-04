18767cc4e6f27b7a2d7720a241d0ec18d55b5d7e
ClémentL <clement.lidar@gmail.com>
2023-08-04 20:16:32 +0200

    Phase 10 : ORM filtrage des enregistrements

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,6 +35,21 @@ class VoyagesController extends AbstractController{
     
     public function sort($champ, $ordre): Response{
         $visites = $this->repository->findallOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    
+    /**
+     * @route("/voyages/recherche/{champ}", name="voyages.findallequal")
+     * @param type $champ
+     * @param Request $request
+     * @return Response
+     */
+    
+    public function findAllEqual($champ, Request $request): Response {
+        $valeur = $request->get("recherche");
+        $visites = $this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig", [
             'visites' => $visites
         ]);
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -52,6 +52,29 @@ class VisiteRepository extends ServiceEntityRepository
                 ->getQuery()
                 ->getResult();
     }
+    /**
+     * 
+     * Retourne les visistes egales à un parametre
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
+    
+    public function findByEqualValue($champ, $valeur) :array {
+        if($valeur == "") {
+            return $this->createQueryBuilder("v")
+                    ->orderBy('v.'.$champ, 'ASC')
+                    ->getQuery()
+                    ->getResult();
+        } else {
+            return $this->createQueryBuilder('v')
+                    ->where('v.'.$champ.'=:valeur')
+                    ->setParameter('valeur', $valeur)
+                    ->orderBy('v.datecreation', 'DESC')
+                    ->getQuery()
+                    ->getResult();
+        }
+    }
 
 //    /**
 //     * @return Visite[] Returns an array of Visite objects
